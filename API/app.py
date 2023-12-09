fe01177b90c3256ad5d8d23ae200ef52f34bf2d2
Pedro Penteado <106943485+Dreppo@users.noreply.github.com>
2023-12-08 21:37:55 -0300

    Add files via upload
    aggregate

--- API/app.py
+++ API/app.py
@@ -41,7 +41,9 @@ def index():
     "message": "Bem vindo a API", 
     "/plantas": "Retorna o banco completo",
     "/solo": "Retorna os dados do solo",
-    "/nomes": "Retorna o nome_cientifico e nome_popular"
+    "/nomes": "Retorna o nome_cientifico e nome_popular",
+    "/dificuldades": "Retorna o todas as dificuldades que as plantas tem",
+    "/dificuldades/<Dificuldade>": "Retorna o todas as plantas que tem a dificuldade <Dificuldade>"
     })
 
 
@@ -53,12 +55,13 @@ def busca_plantas():
         return jsonify(dados_lista), 200, {'Content-Type': 'application/json; charset=utf-8'}  # 200 OK
     except Exception as e:
         return jsonify({"error": str(e)}), 500, {'Content-Type': 'application/json; charset=utf-8'}  # 500 Internal Server Error
+    
 
 
 
 @app.route('/solo')
 def busca_solo():
-    solos = [
+    pipeline = [
         {
             '$group': {
                 '_id': None,
@@ -73,7 +76,7 @@ def busca_solo():
         }
     ]
 
-    dados = colecao.aggregate(solos)
+    dados = colecao.aggregate(pipeline)
 
     solos_unicos = next(dados)['solos_unicos']
 
@@ -85,5 +88,62 @@ def busca_nome():
     dados_lista = list(dados)
     return jsonify(dados_lista)
 
+@app.route('/dificuldades/<dificuldade>')
+def busca_dificuldade(dificuldade):
+    pipeline = [
+        {
+            '$match': {
+                'dificuldade_cultivar': dificuldade
+            }
+        },
+        {
+            '$project': {
+                'dificuldade_cultivar' : 1,
+                '_id': 0,
+                'nome_cientifico': 1,
+                'nome_popular': 1,
+                'clima': 1,
+                'regiao': 1,
+                'ml_dia': 1,
+                'melhor_solo': 1,
+            },
+        }
+        
+    ]
+
+    resultado = list(colecao.aggregate(pipeline))
+    
+    resposta_json = json.dumps({'Todas as plantas com a dificuldade': dificuldade, 'resultados': resultado}, ensure_ascii=False)
+
+    return resposta_json
+
+@app.route('/dificuldades')
+def mostrar_dificuldade():
+    pipeline = [
+        {
+            '$group': {
+                '_id': None,
+                'dificuldades_unicas': {'$addToSet': '$dificuldade_cultivar'}
+            }
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'dificuldades_unicas': 1
+            }
+        }
+    ]
+
+    dados = colecao.aggregate(pipeline)
+
+    dificuldades_unicas = next(dados)['dificuldades_unicas']
+    
+    resposta_json = json.dumps({'Todos os Tipos de dificuldades': dificuldades_unicas}, ensure_ascii=False)
+
+    return resposta_json
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
